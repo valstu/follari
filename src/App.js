65ba1b8d1836e7c82844b05b2204089c38bd232c
Valtteri Karesto <valtteri.karesto@gmail.com>
2019-06-27 11:03:41 +0300

    [hotfix]: Stopcode missing from new stop, default them to null

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
-import React from "react";
-import { InMemoryCache, ApolloClient } from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
-import { RestLink } from "apollo-link-rest";
+import React from 'react';
+import { InMemoryCache, ApolloClient } from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import { RestLink } from 'apollo-link-rest';
 import camelCase from 'camelcase';
 import Home from './components/Home';
 
-const typePatcher =  {
-  RacksPayload: (data) => {
+const typePatcher = {
+  RacksPayload: data => {
     if (data.racks === null) {
       return data;
     }
 
     const racksArr = Object.keys(data.racks).map(key => {
       if (data.racks[key] === null) {
-        return null
+        return null;
       }
-      return {...data.racks[key], __typename: 'Rack'}
+      return {
+        ...data.racks[key],
+        __typename: 'Rack',
+        stopCode: data.racks[key].stopCode || null,
+      };
     });
+
     // eslint-disable-next-line no-param-reassign
     data.racks = racksArr.filter(Boolean);
     return data;
-  } 
+  },
 };
 
 const restLink = new RestLink({
   uri: 'https://data.foli.fi/',
   typePatcher,
-  fieldNameNormalizer: (key) => camelCase(key),
+  fieldNameNormalizer: key => camelCase(key),
 });
-
-
 
 const client = new ApolloClient({
   link: restLink,
@@ -41,6 +44,5 @@ const App = () => (
     <Home />
   </ApolloProvider>
 );
-
 
 export default App;
